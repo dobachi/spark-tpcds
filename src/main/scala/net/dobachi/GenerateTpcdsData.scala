f3481aa59c0349b069fd0a525b09641bf113e594
dobachi <dobachi1983oss@gmail.com>
2017-02-18 16:51:47 +0900

    Initial implementation

---
+++ src/main/scala/net/dobachi/GenerateTpcdsData.scala
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package net.dobachi
+
+import org.apache.log4j.LogManager
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Generate data of TPC-DS
+  */
+object GenerateTpcdsData {
+
+  val log = LogManager.getRootLogger
+
+  // Scopt configuration
+  case class Config(partitionNum: Int = 10,
+                    toolDir: String = "/usr/local/tpc-ds/default/tools",
+                    scaleFactor: Int = 1,
+                    outputDir: String = "/tmp/tpcds",
+                    appName: String = "GenerateTpcdsData",
+                    enableOverwrite: Boolean = false)
+
+  def main(args: Array[String]): Unit = {
+    val parser = new scopt.OptionParser[Config]("GenerateTpcdsData") {
+      head("GenerateTpcdsData")
+
+      arg[String]("outputDir").action((x, c) =>
+        c.copy(outputDir = x)).text("The URL of output files")
+
+      opt[Int]("partitionNum").action((x, c) =>
+        c.copy(partitionNum = x)).text("The number of partitions, which is also used to configure the parallelism. default: 10")
+
+      opt[String]("toolDir").action((x, c) =>
+        c.copy(toolDir = x)).text("The path of TPC-DS tools directory which contains dsdgen. default: /usr/local/tpc-ds/default/tools")
+
+      opt[Int]("scaleFactor").action((x, c) =>
+        c.copy(scaleFactor = x)).text("The scaleFactor, which is one of parameters of dsdgen. default: 1")
+
+      opt[String]("appName").action((x, c) =>
+        c.copy(appName = x)).text("The name of application which is used for YARN")
+
+      opt[Unit]("enableOverwrite").action((_, c) =>
+        c.copy(enableOverwrite = true)).text("Enable overwrite mode")
+    }
+
+    parser.parse(args, Config()) match {
+      case Some(config) =>
+        implicit val spark = SparkSession.builder().appName(config.appName).getOrCreate()
+        val tpcdsData = new TpcdsData(config.partitionNum, config.toolDir, config.scaleFactor, config.outputDir)
+        tpcdsData.save()
+      case None =>
+        sys.exit(1)
+    }
+  }
+
+}
---
+++ src/main/scala/net/dobachi/TpcdsData.scala
@@ -0,0 +1,74 @@
+package net.dobachi
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.functions._
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+
+
+import scala.sys.process._
+
+/**
+  * Created by dobachi on 2017/02/18.
+  */
+class TpcdsData(partitionNum: Int, toolDir: String, scaleFactor: Int, outputDir: String,
+                enableOverwrite: Boolean = false)(implicit spark: SparkSession)
+extends Serializable {
+
+  val maxIndex = partitionNum - 1
+  val baseRdd = spark.sparkContext.parallelize(0 to maxIndex, partitionNum)
+  val dsdgenBin = s"$toolDir/dsdgen"
+
+  val tables = new Tables()
+
+  val dsdgenRdds = tables.definitions.map { table =>
+    val dsdgenRdd = baseRdd.flatMap{ i =>
+      if (! new java.io.File(dsdgenBin).exists) {
+        sys.error(s"Could not find dsdgen at $dsdgenBin. Run install")
+      }
+
+      val commands = Seq(
+        "bash", "-c",
+        s"cd $toolDir && ./dsdgen -table ${table.name} -filter Y -scale $scaleFactor -RNGSEED 100 -quiet Y")
+      println(commands)
+      commands.lineStream
+    }
+    dsdgenRdd.setName(s"${table.name}, sf=$scaleFactor, strings")
+
+    val splittedRdd = dsdgenRdd.map { str =>
+      val values = str.split("\\|", -1).dropRight(1).map {
+        case "" => null
+        case any: String => any
+      }
+      Row.fromSeq(values)
+    }
+
+    (table, splittedRdd)
+  }
+
+  val dsdgenDFs = dsdgenRdds.map { case (table, rdd) =>
+    val stringDF = spark.sqlContext.createDataFrame(rdd, StructType(table.schema.fields.map(f => StructField(f.name, StringType))))
+    val columns = table.schema.fields.map { f =>
+      col(f.name).cast(f.dataType).as(f.name)
+    }
+    val withSchemaDF = stringDF.select(columns: _*)
+    (table, withSchemaDF)
+  }
+
+  def save() = {
+    if (enableOverwrite) {
+      dsdgenDFs.foreach { case (table, df) =>
+        val outputURL = outputDir + "/" + table.name
+        df.write.mode("overwrite").format("parquet").save(outputURL)
+      }
+    } else {
+      dsdgenDFs.foreach { case (table, df) =>
+        val outputURL = outputDir + "/" + table.name
+        df.write.format("parquet").save(outputURL)
+      }
+    }
+  }
+}
