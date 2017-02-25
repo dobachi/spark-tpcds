a6ec4eb9560c011a576460f6fbe2a7c998ed9677
dobachi <dobachi1983oss@gmail.com>
2017-02-26 01:05:20 +0900

    Remove tab

--- src/main/resources/queries/spark/query06.sql
+++ src/main/resources/queries/spark/query06.sql
@@ -1,4 +1,3 @@
-
 select  a.ca_state state, count(*) cnt
  from customer_address a
      ,customer c
@@ -6,21 +5,21 @@ select  a.ca_state state, count(*) cnt
      ,date_dim d
      ,item i
  where       a.ca_address_sk = c.c_current_addr_sk
- 	and c.c_customer_sk = s.ss_customer_sk
- 	and s.ss_sold_date_sk = d.d_date_sk
- 	and s.ss_item_sk = i.i_item_sk
- 	and d.d_month_seq = 
- 	     (select distinct (d_month_seq)
- 	      from date_dim
+     and c.c_customer_sk = s.ss_customer_sk
+     and s.ss_sold_date_sk = d.d_date_sk
+     and s.ss_item_sk = i.i_item_sk
+     and d.d_month_seq =
+          (select distinct (d_month_seq)
+           from date_dim
                where d_year = 2000
- 	        and d_moy = 2 )
- 	and i.i_current_price > 1.2 * 
-             (select avg(j.i_current_price) 
- 	     from item j 
- 	     where j.i_category = i.i_category)
+             and d_moy = 2 )
+     and i.i_current_price > 1.2 *
+             (select avg(j.i_current_price)
+          from item j
+          where j.i_category = i.i_category)
  group by a.ca_state
  having count(*) >= 10
- order by cnt 
+ order by cnt
  limit 100;
 
 
